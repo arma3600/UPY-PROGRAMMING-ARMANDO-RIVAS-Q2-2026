f5613d4eeb78a2ec8ab351ce07c277f0ef30c1a0
Armando Alberto Rivas Quevedo <2409211@upy.edu.mx>
2026-07-03 13:09:24 -0600

    Add Classwork 12 - The Mandelbrot Set

---
+++ Classwork-12-The-Mandelbrot-Set/mandelbrot2.py
@@ -0,0 +1,39 @@
+from PIL import Image
+
+config = {}
+
+archivo = open("config.txt", 'r')
+
+for linea in archivo:
+    clave, valor = linea.strip(). split("=")
+    config[clave] = float(valor) if "." in valor else int(valor)
+    
+archivo.close()
+
+print(config)
+
+#print(config)
+with open("clase.csv" , 'r') as data:
+    datos = data.readlines() #
+    
+alto, ancho, max_iter = config["alto"], config["ancho"], config["max_iter"]
+    
+img = Image.new('HSV', (ancho, alto))
+
+# quitar encabezados
+encabezados = datos.pop(0)
+
+#print(encabezados)
+for dato in datos:
+    #fila, columna, iteraciones = dato.strip().split(",")
+    fila, columna, iteraciones = map(int, dato.strip().split(","))
+    brillo = 0 if (iteraciones == max_iter) else int ((iteraciones / max_iter) * 255)
+    # putpixel necesita tuplas, la primera me indica la posicion y la segunda el color
+    img.putpixel ((columna,fila), (brillo, 255, 255))
+    
+img_rgb = img.convert('RGB')
+img_rgb.save("mandelbrot-clase.png")
+
+print("DONE")
+    
+    
